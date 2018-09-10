88829a6e28218982d017f71af77072d8936bb204
whgloveucq <whgloveu@hotmail.com>
2018-09-10 17:52:26 +0800

    test

--- DataStructures/datastructers_test.py
+++ DataStructures/datastructers_test.py
@@ -182,3 +182,11 @@ print(a < b == c)
 string1, string2, string3 = '', 'Trondheim', 'Hammer Dance'
 non_null = string1 or string2 or string3
 print(non_null)
+print((1, 2, 3)< (1, 2, 4))
+print([1, 2, 3]< [1, 2, 4])
+print('ABC' < 'C' < 'Pascal' < 'Python')
+
+print((1, 2, 3, 4) < (1, 2, 4))
+print((1, 2)< (1, 2, -1))
+print((1, 2, 3) == (1.0, 2.0, 3.0))
+print((1, 2, ('aa', 'ab'))< (1, 2, ('abc', 'a'), 4))
