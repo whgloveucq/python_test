dee9b99a4c51861c2eee384aa9a48f08b67e55e7
whgloveucq <whgloveu@hotmail.com>
2018-09-14 16:22:17 +0800

    test

---
+++ Modules/module_test.py
@@ -0,0 +1,17 @@
+
+
+import sys
+# print(dir(fib(2)))
+# print("---------")
+# print(dir())
+a = [1, 2, 3, 4, 5]
+from Modules.fibo import *
+fib2 = fib(20)
+print(dir())
+import builtins
+print(dir(builtins))
+print("---------")
+import winsound
+print(dir(sys))
+
+
